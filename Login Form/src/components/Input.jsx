3424439e26977b93ac4f3ce09c23b62da0c0ce9a
Thamarai Selvan <99986869+selvan-28@users.noreply.github.com>
2022-02-19 16:39:01 +0530

    Add files via upload

---
+++ Login Form/src/components/Input.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Input(props, btn) {
+  return <input type={props.type} placeholder={props.placeholder} />;
+  <btn type={btn.type} />;
+}
+
+export default Input;
